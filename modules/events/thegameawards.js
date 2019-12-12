1c41bf0821dcd71a3aeb1ebf122feca404b0ec11
Lasse Støjier Pedersen <quer@quers.net>
2019-12-12 20:45:10 +0100

    Update thegameawards.js
    fix error

--- modules/events/thegameawards.js
+++ modules/events/thegameawards.js
@@ -1,35 +1,26 @@
 var cheerio = require('cheerio');
 module.exports = function(steamClient, RequestCommunity, RequestStore, SessionID, options, callback){
-    //getAuthwgtoken(RequestCommunity, function (authwgtoken) { // we doent need it.
-        FolloworignoreEvent(RequestStore, SessionID, 1, authwgtoken, function () {
-            FolloworignoreEvent(RequestStore, SessionID, 2, authwgtoken, function () {
-                FolloworignoreEvent(RequestStore, SessionID, 3, authwgtoken, function () {
-                    UnfolloworunignoreEvent(RequestStore, SessionID, 0, authwgtoken, function () {
-                        callback();
-                    })
+    FolloworignoreEvent(RequestStore, SessionID, 1, function () {
+        FolloworignoreEvent(RequestStore, SessionID, 2, function () {
+            FolloworignoreEvent(RequestStore, SessionID, 3, function () {
+                UnfolloworunignoreEvent(RequestStore, SessionID, 0, function () {
+                    callback();
                 })
             })
         })
-    //})
-};
-function getAuthwgtoken(RequestStore, callback) {
-    RequestStore.get('https://store.steampowered.com/sale/thegameawards', function (error, response, body) {
-        var $ = cheerio.load(body);
-        var dataUserInfo = JSON.parse($("#application_config").attr("data-userinfo"));
-        callback(dataUserInfo.authwgtoken);
     })
-}
+};
 
-function FolloworignoreEvent(RequestStore, SessionID, notification_flag, authwgtoken, callback) {
+function FolloworignoreEvent(RequestStore, SessionID, notification_flag, callback) {
     var url = "https://store.steampowered.com/events/followorignoreevent";
-    event(RequestStore, SessionID, notification_flag, authwgtoken, url, callback);
+    event(RequestStore, SessionID, notification_flag, url, callback);
 }
-function UnfolloworunignoreEvent(RequestStore, SessionID, notification_flag, authwgtoken, callback) {
+function UnfolloworunignoreEvent(RequestStore, SessionID, notification_flag, callback) {
     var url = "https://store.steampowered.com/events/unfolloworunignoreevent";
-    event(RequestStore, SessionID, notification_flag, authwgtoken, url, callback);
+    event(RequestStore, SessionID, notification_flag, url, callback);
 }
 
-function event(RequestStore, SessionID, notification_flag, authwgtoken, url, callback) {
+function event(RequestStore, SessionID, notification_flag, url, callback) {
     var form = {
         sessionid: SessionID,
         authwgtoken: "not needed?",
