75193361e1f65ab6e78fc148fc09de4eb460ecbf
Lasse Støjier Pedersen <quer@quers.net>
2019-12-22 17:18:25 +0100

    sale queue, to ensure we get all item
    this have not been fully tested, but work when runing. it will ensure that each account have god 3 items. if not it will run up to 3 time more, to ensure we try to get the item

---
+++ modules/events/salequeue.js
@@ -0,0 +1,67 @@
+module.exports = function(steamClient, RequestCommunity, RequestStore, SessionID, options, callback){
+    getNewItems(RequestCommunity, function (itemBefore) {
+        //run queue 3 times to get items
+        queueRun(steamClient, RequestCommunity, RequestStore, SessionID, options, function () {
+            queueRun(steamClient, RequestCommunity, RequestStore, SessionID, options, function () {
+                queueRun(steamClient, RequestCommunity, RequestStore, SessionID, options, function () {
+                    getNewItems(RequestCommunity, function (itemAfter) {
+                        if(itemBefore + 3 == itemAfter)
+                        {
+                            setTimeout(() => {
+                                callback();
+                            }, 10000);// wait 10 sec before, start new module   
+                        }
+                        else
+                        {
+                            var itemNeeded = (itemBefore + 3) - itemAfter;
+                            console.log(steamClient.steamID + " - " + options.UserName + ": might need " + itemNeeded + " items. will try to get it");
+                            
+                            runToGetItem(steamClient, RequestCommunity, RequestStore, SessionID, options, itemNeeded, function () {    
+                                setTimeout(() => {
+                                    callback();
+                                }, 10000);// wait 10 sec before, start new module   
+                            })
+                        }
+                    })
+                })
+            })    
+        })
+    });
+}
+function queueRun(steamClient, RequestCommunity, RequestStore, SessionID, options, callback) {
+    setTimeout(() => {
+        require('../queue')(steamClient, RequestCommunity, RequestStore, SessionID, options, callback);        
+    }, 10000);// 10 sec
+}
+//after run for the 3 items, and we did not get what we shoud this methode will retry 3 times to get the missing item
+function runToGetItem(steamClient, RequestCommunity, RequestStore, SessionID, options, needed, callback) {
+    var fullLoop = needed + 3;
+    var foundItems = 0;
+    var loop = function (loopIndex, whenDone) {
+        if(loopIndex > fullLoop || foundItems == needed){
+            console.log(steamClient.steamID + " - " + options.UserName + ": might not have been able to get all the items");
+            whenDone();
+        }else{
+            getNewItems(RequestCommunity, function (itemBefore) {
+                queueRun(steamClient, RequestCommunity, RequestStore, SessionID, options, function () {
+                    getNewItems(RequestCommunity, function (itemAfter) {
+                        if(itemBefore + 1 == itemAfter){
+                            foundItems += 1;
+                        }
+                        loop(++loopIndex, whenDone);
+                    });
+                });
+            });
+        }
+    }
+    loop(0, callback)
+}
+
+function getNewItems(RequestCommunity, callback) {
+    RequestCommunity.get('https://steamcommunity.com/actions/GetNotificationCounts', function (error, response, body) {
+        //console.log(body);    
+        var data = JSON.parse(body);
+        console.log("new items: "+ data.notifications["5"]);
+        callback(data.notifications["5"]);
+    })
+}
