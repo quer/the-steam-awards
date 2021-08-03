fe5e5a94ac86d30a0c668ace4adea4d06ec1c19a
Lasse Støjier Pedersen <quer@quers.net>
2021-08-03 12:14:30 +0200

    Update guideVoteLikeShare.js
    add so it also will do the vistited count

--- modules/guideVoteLikeShare.js
+++ modules/guideVoteLikeShare.js
@@ -1,10 +1,21 @@
 module.exports = function(steamClient, RequestCommunity, RequestStore, SessionID, options, callback){
     var id = "923012519"; //chace for the guide id
     var appid = "753"; // must be the appid for the guide
-    
-    Rate(id, function () {
-        Favorite(id, appid, callback)
+    LoadGuide(id, function () {
+        Rate(id, function () {
+            Favorite(id, appid, callback)
+        })
     })
+
+    function LoadGuide(id, callback) {
+        RequestCommunity.get({
+            url: "https://steamcommunity.com/sharedfiles/filedetails/?id="+id
+        }, function (error, response, body) {
+            setTimeout(function () {
+                callback();
+            }, 500);
+        });
+    }
 
     function Rate(id, callback) {
         var voteUrl = "https://steamcommunity.com/sharedfiles/voteup";
