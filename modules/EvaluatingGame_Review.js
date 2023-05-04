246f150e12b9520f46bbe157404f4fb6b401beb2
Lasse Støjier Pedersen <quer@quers.net>
2023-05-04 20:52:53 +0200

    Update EvaluatingGame_Review.js
    Ensure we doent get stuck, if response is not valid. and added messages if not valid.

--- modules/EvaluatingGame_Review.js
+++ modules/EvaluatingGame_Review.js
@@ -6,6 +6,7 @@ var EvaluteTypes = {
 }
 //settings start
 var EvaluatingList = [
+    {ID: "104131150", EvaluteType: EvaluteTypes.Yes}
 ];
 //settings end
 module.exports = async function(steamClient, RequestCommunity, RequestStore, SessionID, options, callback){
@@ -13,52 +14,84 @@ module.exports = async function(steamClient, RequestCommunity, RequestStore, Ses
         const evaluateingReview = EvaluatingList[i];
         try {
             if(evaluateingReview.EvaluteType == EvaluteTypes.Yes){
-                await RateGameReview(RequestCommunity, SessionID, evaluateingReview.ID, true);
+                await RateGameReview(evaluateingReview.ID, true, 0);
             }else if(evaluateingReview.EvaluteType == EvaluteTypes.No){
-                await RateGameReview(RequestCommunity, SessionID, evaluateingReview.ID, false);
+                await RateGameReview(evaluateingReview.ID, false, 0);
             }else if(evaluateingReview.EvaluteType == EvaluteTypes.Funny){
-                await RateVoteGagGameReview(RequestCommunity, SessionID, evaluateingReview.ID, true);
+                await RateVoteGagGameReview(evaluateingReview.ID, true, 0);
             }else if(evaluateingReview.EvaluteType == EvaluteTypes.RemoveVote){
                 //we need to rate it funny, to only have i as funny, as you can unset that rate after.
-                await RateVoteGagGameReview(RequestCommunity, SessionID, evaluateingReview.ID, true);
+                await RateVoteGagGameReview(evaluateingReview.ID, true, 0);
                 // then remove the rating. 
-                await RateVoteGagGameReview(RequestCommunity, SessionID, evaluateingReview.ID, false);
+                await RateVoteGagGameReview(evaluateingReview.ID, false, 0);
             }
         } catch (error) {
-            options.logError(options.accountPretty + " Evaluating game review failed, on id: "+ evaluateingReview.ID);
+            options.logError("Evaluating game review failed, on id: "+ evaluateingReview.ID);
             options.logError(error);
         }
     }
     callback();
         
     //the same as RateDownGameReview(), just have rateUp as true
-    function RateGameReview(RequestCommunity, SessionID, GameReviewID, RateMode) {
-        return new Promise(function (resolve, reject) {
+    function RateGameReview(GameReviewID, RateMode, retryTimes) {
+        return new Promise( function (resolve, reject) {
             RequestCommunity.post({
                 url: "https://steamcommunity.com/userreviews/rate/" + GameReviewID,
                 form:{
                     sessionid: SessionID,
                     rateup: RateMode
                 }
-            }, function (error, response, body) {
+            }, async function (error, response, body) {
                 if(error){
                     reject(error);
                     return;
                 }
                 try {
                     var response = JSON.parse(body);
-                    if(response && response.success == 1){
-                        resolve();
-                        return;
+                    if(response){
+                        if ( response.success == 1 )
+                        {
+                            //VALID!!
+                        }
+                        else if ( response.success == 21 )
+                        {
+                            throw 'You must be logged in to perform that action.';
+                        }
+                        else if ( response.success == 17){
+                            if(retryTimes < 3){
+                                await RateGameReview(GameReviewID, RateMode, ++retryTimes);
+                            }else {
+                                throw 'steam might be down, was not able to do Evaluating, will be skippec'    
+                            }
+                        }
+                        else if ( response.success == 15 )
+                        {
+                            throw 'Your account does not have sufficient privileges to perform this action.';
+                        }
+                        else if ( response.success == 24 )
+                        {
+                            throw 'Your account does not meet the requirements to use this feature. https://help.steampowered.com/en/wizard/HelpWithLimitedAccount (Visit Steam Support) for more information.';
+                        }
+                        else
+                        {
+                            throw 'There was an error trying to process your request: ' + response.success;
+                        }
+                    }else{
+                        throw "Response is not json"
                     }
+                    resolve();
+                    
+                    return;
                 } catch (error) {
+                    
                     reject(error);
                     return;
                 }
+                
             });
         })
     }
-    function RateVoteGagGameReview(RequestCommunity, SessionID, GameReviewID, RateMode) {
+    function RateVoteGagGameReview(GameReviewID, RateMode, retryTimes) {
         return new Promise(function (resolve, reject) {
             RequestCommunity.post({
                 url: "https://steamcommunity.com/userreviews/votetag/" + GameReviewID,
@@ -67,22 +100,58 @@ module.exports = async function(steamClient, RequestCommunity, RequestStore, Ses
                     rateup: RateMode,
                     tagid: 1
                 }
-            }, function (error, response, body) {
+            }, async function (error, response, body) {
                 if(error){
                     reject(error);
                     return;
                 }
                 try {
                     var response = JSON.parse(body);
-                    if(response && response.success == 1){
-                        resolve();
-                        return;
+                    if(response){
+                        if ( response.success == 1 )
+                        {
+                            //VALID!!
+                        }
+                        else if ( response.success == 21 )
+                        {
+                            throw 'You must be logged in to perform that action.';
+                        }
+                        else if ( response.success == 17){
+                            if(retryTimes < 3){
+                                await RateVoteGagGameReview(GameReviewID, RateMode, ++retryTimes);
+                            }else {
+                                throw 'steam might be down, was not able to do Evaluating, will be skippec'    
+                            }
+                        }
+                        else if ( response.success == 15 )
+                        {
+                            throw 'Your account does not have sufficient privileges to perform this action.';
+                        }
+                        else if ( response.success == 24 )
+                        {
+                            throw 'Your account does not meet the requirements to use this feature. https://help.steampowered.com/en/wizard/HelpWithLimitedAccount (Visit Steam Support) for more information.';
+                        }
+                        else
+                        {
+                            throw 'There was an error trying to process your request: ' + response.success;
+                        }
+                    }else{
+                        throw "Response is not json"
                     }
+                    resolve();
+                    
+                    return;
                 } catch (error) {
+                    
                     reject(error);
                     return;
                 }
             });
+        })
+    }
+    function HandleResponse(error, response, body, retryObj) {
+        return new Promise(function (resolve, reject) {
+            
         })
     }
 }
